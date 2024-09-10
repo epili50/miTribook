e69180c10a4b2e5d4cc7b94939f8a8e2027336e7
epili50 <epili50@gmail.com>
2024-09-10 21:33:01 +0200

    Schema de Apartment completo

--- models/apartment.model.js
+++ models/apartment.model.js
@@ -7,11 +7,72 @@ const apartmentSchema = new Schema({
         required: true
     },
 
+    city: {
+        type: String,
+        required: true
+    },
+    location: {
+      type: {
+        type: String,  // Será un tipo 'Point'
+        enum: ['Point'], // Debe ser 'Point'
+        default: 'Point'
+      },
+      coordinates: {
+        type: [Number], // Array de números: [longitud, latitud]
+        
+      }
+    },
+
+    beds:{
+        type: Number,
+        required: true
+    },
+
+    bathrooms:{
+        type: Number,
+        required: true
+    },
+
+    rooms:{
+        type: Number,
+        required: true
+    },
+
+
+
     description: {
         type: String,
         required: true
     },
     
+    rules:{
+        type:[String],
+        enum:['Fumar', 'Mascotas', 'Fiestas', 'Invitados']
+    },
+
+    photos: {
+        type: [{
+            photo: {
+                type: String, // URL de la foto
+                required: true,
+            },
+            descriptionPhoto: {
+                type: String,
+                maxlength: 50  // Máximo 50 caracteres
+            }
+        }],
+        validate: {
+            validator: function(v) {
+                return v.length <= 4; // Verifica que el array tenga como máximo 4 elementos
+            },
+            message: 'El array de fotos debe tener como máximo 4 elementos.'
+        }
+    },
+
+    maxGuest:{
+        type: Number,
+        required: true
+    },
 
     price:{
         type: Number,
@@ -27,24 +88,26 @@ const apartmentSchema = new Schema({
         type: String,
         required: true
         // TODO Luego sacar el require y poner que se por defecto la primera del array de fotos
+        // Setter para establecer `mainPhoto` como la primera foto del array `photos`
+        // set: function(value) {
+        //     if (this.photos.length > 0) {
+        //         return this.photos[0].photo; // Usar la primera foto del array como `mainPhoto`
+        //     }
+        //     return value;
+        // }
     },
 
     services:{
         type:[String],
         enum: ['Wifi', 'Aire Acondicionado', 'Calefacción', 'TV', 'Cocina', 'Servicio para Discapacitados']
+        
     }, 
 
-    // services:{
-    //     wifi: Boolean,
-    //     airConditioner: Boolean,
-    //     kitchen: Boolean,
-    //     disability: Boolean,
-    //     heater: Boolean,
-    //     tv: Boolean
-    // }
+   })
 
+// Asegurarse de que el índice geoespacial se cree
+apartmentSchema.index({ location: '2dsphere' });
 
-})
 
 const Apartment = model("Apartment", apartmentSchema);
 
